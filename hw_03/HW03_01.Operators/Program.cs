712aecb1ea2694969b40b122490e70251dff9ad8
KuntsevichKiryl <kun-kir@yandex.ru>
2021-06-18 04:48:47 +0300

    hw_03/task_01: add task realization

---
+++ hw_03/HW03_01.Operators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HW03_01.Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = ReadInput("input a");
+            double b = ReadInput("input b");
+
+            double c = Sum(a, b);
+            Console.WriteLine($"a + b = {c}");
+        }
+
+        private static double ReadInput(string text)
+        {
+            try
+            {
+                Console.WriteLine(text);
+                return Convert.ToDouble(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Incorrect input");
+                System.Environment.Exit(1);
+                return double.NaN;
+            }
+        }
+
+        private static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
