87f1987fa7be870d978803e0fcf2e95ea5a6a183
KuntsevichKiryl <kun-kir@yandex.ru>
2021-06-18 06:44:57 +0300

    hw_03/task_09: add task realization

---
+++ hw_03/HW03_06.Movement/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HW03_06.Movement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("to exit input \"exit\"");
+            while (true)
+            {
+                Movement("where to go");
+            }
+        }
+
+        private static void Movement(string text)
+        {
+            Console.WriteLine(text);
+            string action = Console.ReadLine();
+            switch (action)
+            {
+                case "w":
+                case "W":
+                    Console.WriteLine("forward");
+                    break;
+                case "a":
+                case "A":
+                    Console.WriteLine("left");
+                    break;
+                case "s":
+                case "S":
+                    Console.WriteLine("back");
+                    break;
+                case "d":
+                case "D":
+                    Console.WriteLine("right");
+                    break;
+                case "exit":
+                case "EXIT":
+                    System.Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("stay");
+                    break;
+            }
+        }
+    }
+}
---
+++ hw_03/HW03_07.Birthday/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HW03_07.Birthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input birth yaer");
+            uint year = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("Input birth month number");
+            uint month = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("Input present yaer");
+            uint yearnow = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("Input present month number");
+            uint monthnow = Convert.ToUInt32(Console.ReadLine());
+            if (year > yearnow || month > 12 || month == 0 || monthnow > 12 || monthnow == 0)
+            {
+                Console.WriteLine("incorrect date");
+                return;
+            }
+            uint age = month - monthnow < 0 ? yearnow - year : yearnow - year - 1;
+            Console.WriteLine($"Age: {age}");
+        }
+    }
+}
