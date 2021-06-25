213ddca049d2a0a310a30ba623af4fc1c1c192a9
KuntsevichKiryl <kun-kir@yandex.ru>
2021-06-25 18:43:01 +0300

    hw_04/task_04: add task realization

---
+++ hw_04/HW04_04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW04_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input a poem");
+            string poemString = Console.ReadLine().Replace("O", "A");
+            string[] poem = poemString.Split(";");
+
+            Console.WriteLine("result: ");
+            for (int i = 0; i < poem.Length; i++)
+            {
+                Console.WriteLine(poem[i]);
+            }
+        }
+    }
+}
