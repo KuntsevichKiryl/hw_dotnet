9564f45c9164d7c25132e0b122b6de0247f61967
KuntsevichKiryl <kun-kir@yandex.ru>
2021-07-07 23:10:06 +0300

    HW_05/task_04: add more logical names for sB, sB2, and "upgrade" "check"

--- hw_05/HW05_04/Program.cs
+++ hw_05/HW05_04/Program.cs
@@ -17,27 +17,27 @@ namespace HW05_04
         static string Travel(string add, string zip)
         {
             string[] addMas = add.Split(',');
-            StringBuilder sB = new(zip + ":");
-            StringBuilder sB2 = new("/");
+            StringBuilder zipAndAddress = new(zip + ":");
+            StringBuilder houseNum = new("/");
             bool check = false;
             for (int i = 0; i < addMas.Length; i++)
             {
                 if (addMas[i].Contains(zip))
                 {
-                    if (check == true)
+                    if (check)
                     {
-                        sB2.Append(',');
-                        sB.Append(',');
+                        houseNum.Append(',');
+                        zipAndAddress.Append(',');
                     }
                     int houseInd = addMas[i].IndexOf(' ');
-                    sB2.Append(addMas[i], 0, houseInd);
-                    sB.Append(addMas[i], houseInd + 1, addMas[i].Length - houseInd - 1);
-                    sB.Remove(sB.Length - zip.Length - 1, zip.Length + 1);
+                    houseNum.Append(addMas[i], 0, houseInd);
+                    zipAndAddress.Append(addMas[i], houseInd + 1, addMas[i].Length - houseInd - 1);
+                    zipAndAddress.Remove(zipAndAddress.Length - zip.Length - 1, zip.Length + 1);
                     check = true;
                 }
             }
-            sB.Append(sB2);
-            return (sB.ToString());
+            zipAndAddress.Append(houseNum);
+            return (zipAndAddress.ToString());
         }
     }
 }
