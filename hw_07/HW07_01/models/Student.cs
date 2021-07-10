d6f6bb969f6bcef4586d682be2e7f587d2c3dc27
KuntsevichKiryl <kun-kir@yandex.ru>
2021-07-10 13:45:46 +0300

    hw_07/task_01: add task realization

---
+++ hw_07/HW07_01/models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW07_01.models
+{
+    public class Student : Person
+    {
+        public void ShowAge()
+        {
+            Console.WriteLine("My age is: {0}", Age);
+        }
+        public void GoToClasses()
+        {
+            Console.WriteLine("I'm going to class");
+        }
+    }
+}
---
+++ hw_07/HW07_01/models/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW07_01.models
+{
+    public class Teacher : Person
+    {
+        private string subject = "Csharp";
+        public void Explain()
+        {
+            Console.WriteLine("Our topic today is {0}. Explanation begins", subject);
+        }
+    }
+}
