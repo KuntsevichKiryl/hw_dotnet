0b3bb6664f8f6a644060e29f95e96f6a9b2d6f7b
KuntsevichKiryl <kun-kir@yandex.ru>
2021-07-13 00:59:44 +0300

    hw_07/task_03: add task realization

---
+++ hw_07/HW07_03/models/ComputerPrograms.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW07_03.models
+{
+    class ComputerPrograms : General
+    {
+        public ComputerPrograms(string name, string code, string category, double size) : base(name,
+            code, category, size)
+        { }
+    }
+}
